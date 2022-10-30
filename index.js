91a25e46ea444c7c3bc9c09ed02c21a6b364e23b
unknown <Bogdan.Cerovac@Gmail.com>
2022-10-30 18:09:27 +0100

    Prettier-ified code

--- index.js
+++ index.js
@@ -1,66 +1,55 @@
-const puppeteer = require('puppeteer');
-const { start } = require('repl');
+const puppeteer = require("puppeteer");
 
 (async () => {
-    const url = process.argv[2];
-    if (!url) {
-        throw "Please provide a URL as the first argument";
-    }
-    console.log("URL: ", url);
+  const url = process.argv[2];
+  if (!url) {
+    throw "Please provide a URL as the first argument";
+  }
+  console.log("Analyzing ", url);
 
-    const browser = await puppeteer.launch({
-        headless: true,
-        defaultViewport: null,
-        devtools: true
-    });
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: null,
+    devtools: true,
+  });
 
+  const page = await browser.newPage();
+  page.setCacheEnabled(false);
 
+  // reach out to CDP;
+  const client = await page.target().createCDPSession();
 
+  //console.log(client)
 
-    const page = await browser.newPage()
-    page.setCacheEnabled(false);
+  await client.send("Accessibility.enable");
 
-    // reach out to CDP;
-    const client = await page.target().createCDPSession();
+  //await client.send('Accessibility.loadComplete');
+  client.on("Accessibility.loadComplete", (data) => {
+    console.timeEnd("Accessibility.loadComplete");
+    // console.log('Accessibility.loadComplete', JSON.stringify(data))
+  });
 
-    //console.log(client)
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+  page.on("domcontentloaded", () => {
+    console.timeEnd("DOMContentLoaded_event");
+  });
 
-    await client.send('Accessibility.enable');
+  page.on("load", () => {
+    console.timeEnd("PageLoaded");
+  });
 
-    //await client.send('Accessibility.loadComplete');
-    client.on('Accessibility.loadComplete', (data) => {
+  console.time("PageLoaded");
+  console.time("Accessibility.loadComplete");
+  console.time("DOMContentLoaded_event");
+  await page.goto(url);
 
-        console.timeEnd("Accessibility.loadComplete");
-        // console.log('Accessibility.loadComplete', JSON.stringify(data))
+  // const title = await page.title();
 
-
-    });
-
-
-    // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-    page.on('domcontentloaded', () => {
-        console.timeEnd("DOMContentLoaded_event");
-    });
-
-    
-    page.on('load', () => {
-        console.timeEnd("PageLoaded");
-    });
-
-    console.time("PageLoaded");
-    console.time("Accessibility.loadComplete");
-    console.time("DOMContentLoaded_event");
-    await page.goto(url);
-
-
-
-    // const title = await page.title();
-
-    //console.log("Title of the page is: ", title)
-    // const snapshot = await page.accessibility.snapshot();
-    //console.log("snapshot");
-    //console.log(snapshot);
-    /*
+  //console.log("Title of the page is: ", title)
+  // const snapshot = await page.accessibility.snapshot();
+  //console.log("snapshot");
+  //console.log(snapshot);
+  /*
   
     I thought this would be possible, but it isn't;
   
@@ -72,5 +61,5 @@ const { start } = require('repl');
     
     */
 
-    await browser.close()
-})()
+  await browser.close();
+})();
